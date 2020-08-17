2135de496be1f95fe74c114bf8b15667232496b0
nick bouldien <nick.bouldien@gmail.com>
2020-08-17 14:04:48 -0500

    fetch one tweet working

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -14,11 +15,26 @@ const ApiKey = "API_KEY"
 const ApiSecret = "API_SECRET"
 const ApiToken = "API_TOKEN"
 
-//const apiVersion = 2
+//const twitterApiVersion = 2
 const baseURL = "https://api.twitter.com/2"
 
 //var twitterRules []string
 var apiToken string
+
+// endpoints
+// GET /2/tweets/search/stream/rules
+//
+
+type Tweet struct {
+	Data []TweetData `json:"data"`
+}
+
+type TweetData struct {
+	//CreatedAt string `json:"created_at"`
+	//FullText string `json:"full_text"`
+	ID string `json:"id"`
+	Text string `json:"text"`
+}
 
 func main() {
 	if err := godotenv.Load(); err != nil {
@@ -41,8 +57,8 @@ func main() {
 
 	//updateRules(&twitterRules)
 
-	//url := fmt.Sprintf(baseURL + "/tweets?ids=1261326399320715264")
-	url := "http://example.com/"
+	url := fmt.Sprintf(baseURL + "/tweets?ids=1261326399320715264")
+	//url := "http://example.com/"
 	fmt.Println("url: ", url)
 
 	body, err := fetch(url)
@@ -50,6 +66,13 @@ func main() {
 		fmt.Errorf("error reading the response: %v", err)
 	}
 	fmt.Println("body: ", body)
+
+	var tweet Tweet
+
+	if err := json.Unmarshal([]byte(body), &tweet); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("tweet: ", tweet)
 }
 
 func fetch(url string) ([]byte, error) {
@@ -58,6 +81,8 @@ func fetch(url string) ([]byte, error) {
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	req.Header.Add("Authorization", bearerToken)
 	//resp, err := http.Get(url)
+
+	fmt.Println("req: ", req)
 
 	client := &http.Client{}
 	resp, err := client.Do(req)
