5d5399299ecd803d581f5d09a0bdf472dd8c628c
nick bouldien <nick.bouldien@gmail.com>
2020-08-31 20:23:56 -0500

    add some rudimentary button styles

--- frontend/src/components/ConnectionInfo.vue
+++ frontend/src/components/ConnectionInfo.vue
@@ -43,6 +43,34 @@ export default class ConnectionInfo extends Vue {
 </script>
 
 <style scoped lang="scss">
+/* TODO - extract all of this to a custom component */
+button {
+	background: linear-gradient(to bottom, #f9f9f9 5%, #e9e9e9 100%);
+	background-color: #f9f9f9;
+	box-shadow: inset 0px 1px 0px 0px #ffffff;
+	border: 1px solid #dcdcdc;
+	border-radius: 6px;
+	cursor: pointer;
+	color: #666666;
+	display: inline-block;
+	font-family: Arial;
+	font-size: 15px;
+	font-weight: bold;
+	padding: 6px 24px;
+	text-decoration: none;
+	text-shadow: 0px 1px 0px #ffffff;
+
+  &:hover {
+    background: linear-gradient(to bottom, #e9e9e9 5%, #f9f9f9 100%);
+    background-color: #e9e9e9;
+  }
+
+  &:active {
+    position: relative;
+    top: 1px;
+  }
+}
+
 .closed {
   color: red;
 }
